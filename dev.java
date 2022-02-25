38eb229cd74b785d2eb2c7de5d5c15704bffb475
maeva m <maeva.mbongo001@gmail.com>
2022-02-25 01:10:28 +0000

    script added

--- dev.java
+++ dev.java
@@ -1,1 +1,9 @@
-this file should be moved to the development branch
+#this file should be moved to the development branch
+#file was moved to developmenent environment
+echo "Welcome to TD Bank"
+echo "Your banking partner"
+echo "Please enter your username"
+read username
+echo "please enter your pin"
+read -s pin
+echo "you have entered the correct pin"
